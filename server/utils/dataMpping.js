1a32048118245dec2c95c83d6e5e861b44556057
he1275765531@sohu.com <he1275765531@sohu.com>
2018-08-07 19:30:28 +0800

    --- 数据库字段 与前端字段对应

--- server/utils/dataMpping.js
+++ server/utils/dataMpping.js
@@ -1,22 +1,17 @@
 var dataList = require("../dic/mapping");
 var mappingDic = (res) => {
-  let dataArr = [];
-  res.map((item, index) => {
-    let dataObj = {}; //不能放到全局
-    for (let key in item) {
-      for (let value in dataList) {
+  var dataArr = res.map((item, index) => {
+    var dataObj = {}; //不能放到全局
+    for (var key in item) {
+      for (var value in dataList) {
         if (key == value.toLocaleUpperCase()) {
           dataObj[dataList[value]] = item[key]
         }
       }
     }
-    dataArr.push(dataObj);
-  });
-  if (res.length == 0) {
     return dataObj
-  } else {
-    return dataArr;
-  }
+  });
+  return dataArr;
 };
 module.exports = {
   "mappingDic": mappingDic
